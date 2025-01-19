6bb5853b6671e0421e1daefddfd18db17d78fac4
ABDULDEV <abdulkadiradamu203@gmail.com>
2025-01-19 18:07:44 +0100

    feat: new world

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+// LOGI
+}function openLoginPopup() {
+    document.getElementById("loginPopup").style.display = "flex";
+}
+
+// Function to close the login popup
+function closeLoginPopup() {
+    document.getElementById("loginPopup").style.display = "none";
+}
